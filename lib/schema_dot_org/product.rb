8c96de1317fe8e7e164b6624dc9df287b5939ed2
Nick Gorodnov <ngorodnov@gmail.com>
2019-09-11 20:31:16 +0400

    Fix nil

--- lib/schema_dot_org/product.rb
+++ lib/schema_dot_org/product.rb
@@ -13,11 +13,11 @@ module SchemaDotOrg
 
     validates :name,        type: String, presence: true
     validates :url,         type: String, presence: true
-    validates :sku,         type: String
-    validates :weight,      type: String
-    validates :image,       type: String, presence: true
-    validates :category,    type: String, presence: true
-    validates :description, type: String
+    validates :sku,         type: String, allow_nil: true
+    validates :weight,      type: String, allow_nil: true
+    validates :image,       type: String, allow_nil: true
+    validates :category,    type: String, allow_nil: true
+    validates :description, type: String, allow_nil: true
     validates :offers,      type: Array,  allow_nil: false
 
     def _to_json_struct
